1ac9432b87b70826c77cbbe4ec83d33872de4fce
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-13 23:06:40 +0530

    Link-to navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => (
   <div>
     This is page One!! <br />
+    <Link to="/two">Go to page two</Link>
   </div>
 );
 
 const PageTwo = () => (
   <div>
     This is page Two my boi!! <br />
+    <Link to="/">Go back to page one</Link>
   </div>
 );
 
