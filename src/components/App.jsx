49e76fc454e6acff4dac0919a00df7e1040178f6
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-13 20:45:08 +0530

    The basics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => (
+  <div>
+    This is page One!! <br />
+    <a href="/two">Go to page two</a>
+  </div>
+);
+
+const PageTwo = () => (
+  <div>
+    This is page Two my boi!! <br />
+    <a href="/">Go back to page one</a>
+  </div>
+);
 
 const App = () => (
   <div>
-    This is App! I think...
+    {/* This is App! I think... */}
+    <BrowserRouter>
+      <Route path="/" exact component={PageOne} />
+      <Route path="/two" component={PageTwo} />
+    </BrowserRouter>
   </div>
 );
 
