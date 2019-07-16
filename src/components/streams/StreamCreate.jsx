c13086032e52479d5b711aeacbbdd7d1b77e0145
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-16 16:48:32 +0530

    Introducing Validation

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import PropTypes from 'prop-types';
 
 // Still can make it a dumb component
 class StreamCreate extends Component {
@@ -8,16 +8,36 @@ class StreamCreate extends Component {
     console.log(formValues);
   }
 
-  renderInput = ({ input, label }) => (
-    <div className="field">
-      <label>{label}</label>
-      <input type="text" {...input} />
-    </div>
-  )
+  renderError = ({ error, touched }) => {
+    if (error && touched) {
+      return (
+        <div className="ui error message">
+          <div className="error">
+            {error}
+          </div>
+        </div>
+      );
+    } return null;
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.touched && meta.error ? 'error' : ''}`;
+    const labelId = `createForm${Math.random()}`;
+    return (
+      <div className={className}>
+        <label htmlFor={labelId}>
+          {label}
+          <input id={labelId} type="text" {...input} autoComplete="off" />
+        </label>
+        {this.renderError(meta)}
+      </div>
+    );
+  }
 
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
         <button className="ui button primary" type="submit">Submit</button>
@@ -26,6 +46,24 @@ class StreamCreate extends Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'You must a valid Stream Title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a valid Stream Description';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'streamCreate',
+  validate,
 })(StreamCreate);
+
+StreamCreate.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
