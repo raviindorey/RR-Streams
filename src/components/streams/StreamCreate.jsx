6af3faf1b1db1752f5325e0f48abf7f08ba1f7d0
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-16 15:56:03 +0530

    Introducing handle submit

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 
+// Still can make it a dumb component
 class StreamCreate extends Component {
-  renderInput(formProps) {
-    return (
-      <input
-        type="text"
-        onChange={formProps.input.onChange}
-        value={formProps.input.value}
-      />
-    )
+  onSubmit = (formValues) => {
+    console.log(formValues);
   }
+
+  renderInput = ({ input, label }) => (
+    <div className="field">
+      <label>{label}</label>
+      <input type="text" {...input} />
+    </div>
+  )
 
   render() {
     return (
-      <div>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
-      </div>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary" type="submit">Submit</button>
+      </form>
     );
   }
 }
