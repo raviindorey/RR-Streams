535f5facdf32544bb1481c39f87a37b61e4b8271
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-19 12:02:52 +0530

    Completed delete stream modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import history from '../history';
 
 const Modal = () => ReactDom.createPortal(
-  <div className="ui dimmer modals visible active">
-    <div className="ui standard modal visible active">
-      Hello From Modal!!!
+  <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+    <div onClick={e => e.stopPropagation()} className="ui standard modal visible active">
+      <div className="header">
+        Delete Stream
+      </div>
+      <div className="content">
+        Are you sure you want to delete this stream?
+      </div>
+      <div className="actions">
+        <button className="ui button primary" type="button">
+          Delete
+        </button>
+        <button className="ui button" type="button">
+          Cancel
+        </button>
+      </div>
     </div>
   </div>,
   document.querySelector('#modal'),
