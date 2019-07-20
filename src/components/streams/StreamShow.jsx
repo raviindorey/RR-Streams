d538342114d96dd3546d3503dd9e234f5a5d83a6
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-20 21:41:14 +0530

    Hook up showstream to OBS

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import flv from 'flv.js';
 
 import { fetchStream } from '../../actions';
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     const { fetchStream, match } = this.props;
     fetchStream(match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.flvPlayer.destroy();
+  }
+
+  buildPlayer() {
+    const { stream, match } = this.props;
+    if (this.flvPlayer || !stream) {
+      return;
+    }
+
+    this.flvPlayer = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${match.params.id}.flv`,
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
   }
 
   render() {
@@ -19,6 +48,7 @@ class StreamShow extends Component {
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{stream.title}</h1>
         <h5>{stream.description}</h5>
       </div>
